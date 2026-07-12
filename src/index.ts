343779169c6b59e6f5dd4969b01bf569f78e443e
Jebreal AL_drani <jebrealaldrani@gmail.com>
2026-07-12 18:36:14 +0300

    Implement Get all subjects routes

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response, Express } from "express";
+import subjectsRouter from "./routes/subjects.ts";
+import cors from "cors";
+
+const app: Express = express();
+
+const PORT: number = 8000;
+
+if(!process.env.FRONTEND_URL) {
+    throw new Error("Missing FRONTEND_URL");
+}
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+     credentials: true
+}))
+
+app.use(express.json());
+
+app.use("/api/subjects", subjectsRouter);
+
+app.get("/", (req: Request, res: Response): void => {
+    res.json({
+        message: "Classroom API Run Successfully!"
+    });
+});
+
+if (!PORT) {
+    throw new Error("PORT is not assigned in environment variables");
+}
+
+app.listen(PORT, (): void => {
+    console.log("Server is running successfully on port:", PORT);
+});
---
+++ src/routes/subjects.ts
@@ -0,0 +1,74 @@
+import express, {Router} from "express";
+import {departments, subjects} from "../db/schema";
+
+import {ilike, or, eq, sql, count, and, getTableColumns, desc} from "drizzle-orm"
+import {db} from "../db";
+
+const router: Router = express.Router();
+
+router.get("/", async (req, res) => {
+    try {
+        const {search, department, page = 1, limit = 10} = req.query;
+
+        const currentPage = Math.max(1, +page);
+        const limitPerPage = Math.max(5, +limit);
+
+        const offset = (currentPage - 1) * limitPerPage;
+
+        const filterConditions = [];
+
+        //If search query exists, filter by subject name OR subject code
+        if (search) {
+            filterConditions.push(
+                or(
+                    ilike(subjects.name, `%${search}%`),
+                    ilike(subjects.code, `%${search}%`),
+                )
+            )
+        }
+
+        //If department filter exists, match department name
+        if (department) {
+            filterConditions.push(ilike(departments.name, `%${department}%`));
+        }
+
+        //combine all filters using AND if any exists
+        const whereClause = filterConditions.length > 0 ? and(...filterConditions) : undefined;
+
+        const countResult = await db
+            .select({count: sql<number>`count(*)`})
+            .from(subjects).leftJoin(departments, eq(subjects.departmentId, departments.id))
+            .where(whereClause)
+
+        const totalCount = countResult[0]?.count ?? 0;
+
+        const subjectsList = await db
+            .select({
+                ...getTableColumns(subjects),
+                department: {...getTableColumns(departments)}
+            })
+            .from(subjects)
+            .leftJoin(departments, eq(subjects.departmentId, departments.id))
+            .where(whereClause)
+            .orderBy(desc(subjects.createdAt))
+            .limit(limitPerPage)
+            .offset(offset)
+
+        res.status(200).json({
+            data: subjectsList,
+            pagination: {
+                total: totalCount,
+                totalPages: Math.ceil(totalCount / totalCount),
+                page: currentPage,
+                limit: limitPerPage,
+            }
+        })
+    } catch (err) {
+        console.error(`GET Subjects error : ${err}`)
+        res.status(500).json({
+            message: `GET Subjects error : ${err}`
+        })
+    }
+});
+
+export default router;
